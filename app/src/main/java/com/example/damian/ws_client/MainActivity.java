296535d6088a0c8f2778c93b08a1400c77c3cb90
Damian Banaszkiewicz <banaszkiewicz.db@gmail.com>
2019-02-18 20:21:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/damian/ws_client/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.damian.ws_client;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final String apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonGet = findViewById(R.id.buttonGetActivity);
+        Button buttonPost = findViewById(R.id.buttonPostActivity);
+        Button buttonPut = findViewById(R.id.buttonPutActivity);
+        Button buttonDelete = findViewById(R.id.buttonDeleteActivity);
+
+        buttonGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
